c566da792758b3965297500c24c7b31c48caaf74
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-19 08:54:38 +0530

    Create users.js

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export const users = [
+  {
+    id: 'you',
+    username: 'you',
+    name: 'You',
+    avatar: '/images/dp_you.jpg',
+    bio: 'Frontend dev • Coffee • Photos',
+    following: ['alex','maria']
+  },
+  {
+    id: 'alex',
+    username: 'alex',
+    name: 'Alex Johnson',
+    avatar: '/images/dp_alex.jpg',
+    bio: 'Traveler • Photographer',
+    following: ['maria']
+  },
+  {
+    id: 'maria',
+    username: 'maria',
+    name: 'Maria Lee',
+    avatar: '/images/dp_maria.jpg',
+    bio: 'Foodie • Designer',
+    following: ['alex']
+  }
+]
